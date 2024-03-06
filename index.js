b16625c6149a68306b84099f1d11cb44f622614e
ilechang <117113333+ilechang@users.noreply.github.com>
2024-03-06 09:57:52 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+//Step 1: Run the solution.js file without looking at the code.
+//Step 2: You can go to the recipe.json file to see the full structure of the recipeJSON below.
+const recipeJSON =
+  '[{"id": "0001","type": "taco","name": "Chicken Taco","price": 2.99,"ingredients": {"protein": {"name": "Chicken","preparation": "Grilled"},  "salsa": {"name": "Tomato Salsa","spiciness": "Medium"},  "toppings": [{"name": "Lettuce",  "quantity": "1 cup",  "ingredients": ["Iceberg Lettuce"]  },      {"name": "Cheese",  "quantity": "1/2 cup",  "ingredients": ["Cheddar Cheese", "Monterey Jack Cheese"]  },      {"name": "Guacamole",  "quantity": "2 tablespoons",  "ingredients": ["Avocado", "Lime Juice", "Salt", "Onion", "Cilantro"]  },      {"name": "Sour Cream",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream"]  }      ]    }  },{"id": "0002","type": "taco","name": "Beef Taco","price": 3.49,"ingredients": {"protein": {"name": "Beef","preparation": "Seasoned and Grilled"},  "salsa": {"name": "Salsa Verde","spiciness": "Hot"},  "toppings": [{"name": "Onions",  "quantity": "1/4 cup",  "ingredients": ["White Onion", "Red Onion"]  },      {"name": "Cilantro",  "quantity": "2 tablespoons",  "ingredients": ["Fresh Cilantro"]  },      {"name": "Queso Fresco",  "quantity": "1/4 cup",  "ingredients": ["Queso Fresco"]  }      ]    }  },{"id": "0003","type": "taco","name": "Fish Taco","price": 4.99,"ingredients": {"protein": {"name": "Fish","preparation": "Battered and Fried"},  "salsa": {"name": "Chipotle Mayo","spiciness": "Mild"},  "toppings": [{"name": "Cabbage Slaw",  "quantity": "1 cup",  "ingredients": [    "Shredded Cabbage",    "Carrot",    "Mayonnaise",    "Lime Juice",    "Salt"          ]  },      {"name": "Pico de Gallo",  "quantity": "1/2 cup",  "ingredients": ["Tomato", "Onion", "Cilantro", "Lime Juice", "Salt"]  },      {"name": "Lime Crema",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream", "Lime Juice", "Salt"]  }      ]    }  }]';
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+const recipe = JSON.parse(recipeJSON);
+let i = "";
+
+
+
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/recipe", (req, res) => {
+  //Step 3: Write your code here to make this behave like the solution website.
+  //Step 4: Add code to views/index.ejs to use the recieved recipe object.
+if(req.body.choice === "chicken"){
+  i = 0;
+}else if (req.body.choice === "beef"){
+  i = 1;
+}else{
+  i = 2;
+}
+let name = recipe[i].name;
+let protein = recipe[i].ingredients.protein.name;
+let proteinPrep = recipe[i].ingredients.protein.preparation;
+let salsaName = recipe[i].ingredients.salsa.name;
+//This line of code (map method)will transform specified data and create a new array called toppings
+const toppings = recipe[i].ingredients.toppings.map(item => ({
+  quantity: item.quantity,
+  name: item.name
+}));
+
+
+console.log(toppings)  //可以看看new array長怎樣
+
+
+
+res.render("index.ejs", {
+  name,
+  protein,
+  proteinPrep,
+  salsaName,
+toppings
+})
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
